0427cd983ee5ef4206c5822755550cf8e1399ad7
DaEagle01 <fbinkarim01@gmail.com>
2021-08-21 23:02:20 +0600

    added

--- interact.js
+++ interact.js
@@ -1,19 +1,24 @@
 // interacting with mac book pro
 let ramSmall = document.getElementById("ram-small");
 let ramLarge = document.getElementById("ram-large");
+
 let ssdSmall = document.getElementById("ssd-small");
 let ssdMid = document.getElementById("ssd-mid");
 let ssdLarge = document.getElementById("ssd-large");
+
 let freeDelivery = document.getElementById("free-delivery");
 let expressDelivery = document.getElementById("express-delivery");
+
 let extraRamCost = document.getElementById("extra-ram-cost");
 let extraSsdCost = document.getElementById("extra-ssd-cost");
 let deliveryCost = document.getElementById("extra-delivery-charge");
+
 let totalPrice = document.getElementById("total-price");
 totalPrice.innerText = Number(1299);
 let totalOut = document.getElementById("total");
-let input = document.getElementById('promo-input');
-let promoButton = document.getElementById('promo-button')
+
+let input = document.getElementById("promo-input");
+let promoButton = document.getElementById("promo-button");
 
 ramSmall.addEventListener("click", function () {
   extraRamCost.innerText = 0;
@@ -34,7 +39,8 @@ ssdSmall.addEventListener("click", function () {
 ssdMid.addEventListener("click", function () {
   extraSsdCost.innerText = 100;
   extraStorageCost = extraSsdCost;
-  totalCost = Number(totalPrice.innerText) + parseFloat(extraStorageCost.innerText);
+  totalCost =
+    Number(totalPrice.innerText) + parseFloat(extraStorageCost.innerText);
   totalPrice.innerText = totalCost;
   totalOut.innerText = totalCost;
 });
@@ -42,7 +48,8 @@ ssdMid.addEventListener("click", function () {
 ssdLarge.addEventListener("click", function () {
   extraSsdCost.innerText = 180;
   extraStorageCost = extraSsdCost;
-  totalCost = Number(totalPrice.innerText) + parseFloat(extraStorageCost.innerText);
+  totalCost =
+    Number(totalPrice.innerText) + parseFloat(extraStorageCost.innerText);
   totalPrice.innerText = totalCost;
   totalOut.innerText = totalCost;
 });
@@ -54,17 +61,17 @@ freeDelivery.addEventListener("click", function () {
 expressDelivery.addEventListener("click", function () {
   deliveryCost.innerText = 20;
   expressDeliveryCost = deliveryCost;
-  totalCost = Number(totalPrice.innerText) + parseFloat(expressDeliveryCost.innerText);
+  totalCost =
+    Number(totalPrice.innerText) + parseFloat(expressDeliveryCost.innerText);
   totalPrice.innerText = totalCost;
   totalOut.innerText = totalCost;
 });
 
-promoButton.addEventListener('click', function () {
-  if (input.value == 'stevekaku') {
-    let afterPromoApply = parseFloat(totalOut.innerText)
-    afterPromoApply = afterPromoApply - (afterPromoApply * 0.2);
+promoButton.addEventListener("click", function () {
+  if (input.value == "stevekaku") {
+    let afterPromoApply = parseInt(totalOut.innerText);
+    afterPromoApply = afterPromoApply - afterPromoApply * 0.2;
     totalOut.innerText = afterPromoApply;
-}
-})
-
-
+  }
+  input.value = "";
+});
